226cf94a194a35972889fdda3ff6791f2932ad57
vamsikrishna1007 <leelavamsikrishna1@gmail.com>
2023-11-07 22:52:29 +0530

    deploy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,76 +1,99 @@
 package org.example;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Scanner;
+
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        logger.info("Start of Execution");
-        int option = 0;
-        for(;;) {
-            System.out.println("-----Welcome to Calculator------\n" +
-                    "Choose your operation\n" +
-                    "1. Addition\n" +
-                    "2. Subtraction\n" +
-                    "3. Multiplication\n" +
-                    "4. Division\n" +
-                    "5. Exit");
-            Scanner input = new Scanner(System.in);
-            option = input.nextInt();
-            if(option == 5){
-                logger.info("End of Execution");
-                System.exit(0);
-            }
-            System.out.println("Enter 1st operand: ");
-            int op1 = input.nextInt();
-            System.out.println("Enter 2nd operand: ");
-            int op2 = input.nextInt();
-            switch (option) {
-                case 1:
-                    logger.info("START OP: Add");
-                    System.out.println("--------Result is--------\n" +
-                            op1 + " + " + op2 + " = " + add(op1, op2));
-                    logger.info("END OP: Add");
-                    break;
-                case 2:
-                    logger.info("START OP: Sub");
-                    System.out.println("--------Result is--------\n" +
-                            op1 + " - " + op2 + " = " + sub(op1, op2));
-                    logger.info("END OP: Sub");
-                    break;
-                case 3:
-                    logger.info("START OP: Mul");
-                    System.out.println("--------Result is--------\n" +
-                            op1 + " * " + op2 + " = " + mul(op1, op2));
-                    logger.info("END OP: Mul");
-                    break;
-                case 4:
-                    logger.info("START OP: Div");
-                    System.out.println("--------Result is--------\n" +
-                            op1 + " / " + op2 + " = " + div(op1, op2));
-                    logger.info("END OP: Div");
-                    break;
-                case 5:
-                    System.exit(0);
+        logger.info("Calculator Begins!!");
+        //some changes
+        Scanner reader = new Scanner(System.in);
+        int op, flag=0;
+        int num, exp;
 
-                    break;
-                default:
-                    logger.warn("Invalid input");
+        do{
+            System.out.println("Operation:\n");
+            System.out.println("1. Add");
+            System.out.println("2. Subtract");
+            System.out.println("3. Multiply");
+            System.out.println("4. Percentile");
+            System.out.println("5. Exit\n");
+            System.out.print("Enter your choice(number): \n");
+            op = reader.nextInt();
+            if(op==5) flag = 1;
+            else{
+                switch(op){
+
+                    case 1:  // addition
+                        System.out.println("Add two numbers!! \n");
+                        System.out.print("Enter number1: \n");
+                        num = reader.nextInt();
+                        System.out.print("Enter number2: \n");
+                        exp = reader.nextInt();
+                        add(num,exp);
+                        break;
+
+                    case 2: // Subtract
+                        System.out.println("Subtract two numbers!!\n");
+                        System.out.print("Enter number1: \n");
+                        num = reader.nextInt();
+                        System.out.print("Enter number2: \n");
+                        exp = reader.nextInt();
+                        subt(num,exp);
+                        break;
+
+                    case 3: // Muktiply
+                        System.out.println("Multiply two numbers!!\n");
+                        System.out.print("Enter number1: \n");
+                        num = reader.nextInt();
+                        System.out.print("Enter number2: \n");
+                        exp = reader.nextInt();
+                        mul(num,exp);
+                        break;
+
+
+                    case 4: // Percentile
+                        System.out.println("Percentile two numbers!!\n");
+                        System.out.print("Enter number1: \n");
+                        num = reader.nextInt();
+                        System.out.print("Enter number2: \n");
+                        exp = reader.nextInt();
+                        per(num,exp);
+                        break;
+
+                    default:
+                        System.out.println("Exiting due to invalid input!!");
+                        flag = 1;
+                }
             }
-        }
+        }while(flag == 0);
     }
-    public static int add(int a, int b){
-        return a+b;
+
+    public static int add(int num,int exp){
+        int c=num+exp;
+        System.out.println("\nThe Result is "+c+"\n");
+        logger.info("Executing addition operation for "+num+" and "+exp+". Result: "+c+"\n");
+        return c;
     }
-    public static int sub(int a, int b){
-        return a-b;
+
+    public static int subt(int num,int exp){
+        int c=num-exp;
+        System.out.println("\nThe Result is "+c+"\n");
+        logger.info("Executing subtraction operation for "+num+" and "+exp+". Result: "+c+"\n");
+        return c;
     }
-    public static int mul(int a, int b){
-        return a*b;
+    public static int mul(int num,int exp){
+        int c = num*exp;
+        System.out.println("\nThe Result is "+c+"\n");
+        logger.info("Executing multiplication operation for "+num+" and "+exp+". Result: "+c+"\n");
+        return c;
     }
-    public static int div(int a, int b){
-        return a/b;
+    public static int per(int num, int exp){
+        int c = num%exp;
+        System.out.println("\nThe Result is "+c+"\n");
+        logger.info("Executing percentile operation for "+num+" and "+exp+". Result: "+c+"\n");
+        return c;
     }
 }
