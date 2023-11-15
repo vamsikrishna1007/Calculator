d48b70a7fbab4a29178eadb89a974dabb7722b6d
vamsikrishna1007 <leelavamsikrishna1@gmail.com>
2023-11-15 13:22:23 +0530

    remove space

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,7 +30,7 @@ public class Main {
                         System.out.println("Add two numbers! \n");
                         System.out.print("Enter number1: \n");
                         num = reader.nextInt();
-                        System.out.print("Enter number2: \n");
+                        System.out.print("Enter  number2: \n");
                         exp = reader.nextInt();
                         add(num,exp);
                         break;
